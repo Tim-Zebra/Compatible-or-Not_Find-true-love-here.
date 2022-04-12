a70f15dc632ab109658080803fc75712eea51660
jacef790 <jace.feinstein@live.com>
2022-04-12 09:37:12 -0600

    api added

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const key = 'b0c6ee4c63msh8f0c74e8dacd7e8p1b362djsnfa58527ad9fd';
+
+
+
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'api-baseball.p.rapidapi.com',
+		'X-RapidAPI-Key': 'b0c6ee4c63msh8f0c74e8dacd7e8p1b362djsnfa58527ad9fd'
+	}
+};
+
+
+const leagues = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'api-baseball.p.rapidapi.com',
+		'X-RapidAPI-Key': 'b0c6ee4c63msh8f0c74e8dacd7e8p1b362djsnfa58527ad9fd'
+	}
+};
+
+fetch('https://api-baseball.p.rapidapi.com/leagues?name=mlb', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+    console.log(leagues);
+    console.log(options);
