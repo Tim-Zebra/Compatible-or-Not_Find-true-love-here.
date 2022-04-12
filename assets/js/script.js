3ef189a449039ef455952966ec751ad92836a6be
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-04-12 13:18:47 -0500

    added API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,16 @@
-const key = 'b0c6ee4c63msh8f0c74e8dacd7e8p1b362djsnfa58527ad9fd';
-
-
-
-
+// Get API Key
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Host': 'api-baseball.p.rapidapi.com',
-		'X-RapidAPI-Key': 'b0c6ee4c63msh8f0c74e8dacd7e8p1b362djsnfa58527ad9fd'
+		'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com',
+		'X-RapidAPI-Key': 'd7b52d453cmsh879a19108989e7bp13154cjsn9c9ddd1cbe5d'
 	}
 };
 
-
-const leagues = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'api-baseball.p.rapidapi.com',
-		'X-RapidAPI-Key': 'b0c6ee4c63msh8f0c74e8dacd7e8p1b362djsnfa58527ad9fd'
-	}
-};
-
-fetch('https://api-baseball.p.rapidapi.com/leagues?name=mlb', options)
+fetch('https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
 
-    console.log(leagues);
-    console.log(options);
 
-	// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\Third Page///////////////////////////////////////
-	// Obtain list of players
-	// Obtain stats of players
-	// Compare stats of players
-	// Drop down box to choose players to compare https://jqueryui.com/selectmenu/#default
-	$( function() {
-		$( "#speed" ).selectmenu();
-	 
-		$( "#files" ).selectmenu();
-	});
+	// 
