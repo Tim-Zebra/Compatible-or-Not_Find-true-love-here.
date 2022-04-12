8f6e969f893432a1e9427a3fd1176818e18d45e9
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-04-12 18:24:47 -0500

    added HTML form and JS to submit names. Names submit appropriately. Reorganized JS scripts to work with foundation-css.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,33 +5,86 @@ var weatherAPIObject = null;
 
 // Get love compatibility API
 // example pull https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John'
-const loveAPIObject = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com',
-		'X-RapidAPI-Key': 'd7b52d453cmsh879a19108989e7bp13154cjsn9c9ddd1cbe5d'
-	}
-};
+// Returns both names, a percentage, and some...comment
+// const optionsLove = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com',
+// 		'X-RapidAPI-Key': 'd7b52d453cmsh879a19108989e7bp13154cjsn9c9ddd1cbe5d'
+// 	}
+// };
 
-fetch('https://love-calculator.p.rapidapi.com/getPercentage?', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+// fetch('https://love-calculator.p.rapidapi.com/getPercentage?', optionsLove)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
 
-// Joke API
-// example pull https://www.boredapi.com/api/activity
-const dateAPIObject = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'https://www.boredapi.com/api/activity',
-		'X-RapidAPI-Key': ''
-	}
-};
+// // Joke API
+// // example pull https://www.boredapi.com/api/activity
+// const optionsBored = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Host': 'https://www.boredapi.com/api/activity',
+// 		'X-RapidAPI-Key': ''
+// 	}
+// };
 
-fetch('https://www.boredapi.com/api/activity', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+// fetch('https://www.boredapi.com/api/activity', optionsBored)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
 
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\Form Submission//////////////////////////////////////
+// Sets variable for Form Submissions
+var compatibilityFormEl = $('#compatibilityForm');
+var firstNameEl = $('#firstName');
+var secondNameEl = $('#secondName');
+var compatibilitySectionEl = $('#compatibilitySection');
+
+
+// API result placeholder
+var fetchedScore = 36;
+// Gets input from user
+function getUserInput (event) {
+    event.preventDefault();
+	// defined variables
+    var firstName = '';
+	var secondName = '';
+
+    // Catches if no names are input. If no names, then do nothing
+    if ((firstNameEl.val() !== '') && (secondNameEl.val() !== '')) {
+        // Stores user search as an object
+        var firstName = firstNameEl.val().toLowerCase();
+        var secondName= secondNameEl.val().toLowerCase();
+
+        // Capitalizes first letter of name
+		firstName = capitalizeFirstLetter(firstName);
+		secondName = capitalizeFirstLetter(secondName);
+
+        // Send Results to HTML
+        var pEl = $("<p>" + fetchedScore + "<p>");
+
+        compatibilitySectionEl.append(pEl);
+    }
+    firstNameEl.val('');
+    secondNameEl.val('');
+
+	console.log(firstName, "\n", secondName);
+}
+
+// Takes in a single string of letters
+function capitalizeFirstLetter (word) {
+	var newWord = '';
+	var arr = word.split('');
+
+	arr[0] = arr[0].toUpperCase();
+	newWord = arr.join('');
+
+	return newWord;
+}
+
+// Listens for the form submit button to be pressed
+compatibilityFormEl.on('submit', getUserInput);
 
 
