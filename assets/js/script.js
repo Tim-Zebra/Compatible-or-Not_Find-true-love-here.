e7b309e57d4dcf8a737616249a4499e8d054cf70
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-04-13 11:25:31 -0500

    added joke API function. Successfully pulls a joke excluding inappropriate criteria. Created section on HTML. Did some rearranging of code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,7 @@
 // Global variables for API pull
 var loveAPIObject = ''
 var dateAPIObject = ''
-var weatherAPIObject = ''
-
-// Get love compatibility API
-// example pull https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John'
- compatibilityForm
-// Returns both names, a percentage, and some...comment
-// const optionsLove = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com',
-// 		'X-RapidAPI-Key': 'd7b52d453cmsh879a19108989e7bp13154cjsn9c9ddd1cbe5d'
-// 	}
-// };
-
-// fetch('https://love-calculator.p.rapidapi.com/getPercentage?', optionsLove)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-
-// // Joke API
-// // example pull https://www.boredapi.com/api/activity
-// const optionsBored = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Host': 'https://www.boredapi.com/api/activity',
-// 		'X-RapidAPI-Key': ''
-// 	}
-// };
-
-// fetch('https://www.boredapi.com/api/activity', optionsBored)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-
+var jokeAPIObject = '';
 
 // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\Form Submission//////////////////////////////////////
 // Sets variable for Form Submissions
@@ -88,51 +55,73 @@ function capitalizeFirstLetter (word) {
 // Listens for the form submit button to be pressed
 compatibilityFormEl.on('submit', getUserInput);
 
-const loveAPI = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com',
-		'X-RapidAPI-Key': 'd7b52d453cmsh879a19108989e7bp13154cjsn9c9ddd1cbe5d'
-	}
-};
 
-fetch('https://love-calculator.p.rapidapi.com/getPercentage?', loveAPI)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.then(function (response){
-		loveAPIObject = response;
-		console.log(loveAPIInfo);
-	})
-	.catch(err => console.error(err));
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\API FETCHING/////////////////////////////////////////
+// // Love Compatability API
+getLove();
+function getLove () {
+    const optionsLove = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com',
+            'X-RapidAPI-Key': 'd7b52d453cmsh879a19108989e7bp13154cjsn9c9ddd1cbe5d'
+        }
+    };
+    fetch('https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John', optionsLove)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+}
 
-// Joke API
+// Activity API
 // example pull https://www.boredapi.com/api/activity
-const dateAPI = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'https://www.boredapi.com/api/activity',
-		'X-RapidAPI-Key': ''
-	}
-};
+// const dateAPI = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Host': 'https://www.boredapi.com/api/activity',
+// 		'X-RapidAPI-Key': ''
+// 	}
+// };
 
-fetch('https://www.boredapi.com/api/activity', dateAPI)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.then(function (response){
-		dateAPIObject = response;
-		console.log(dateAPIObject);
-	})
-	.catch(err => console.error(err));
+// fetch('https://www.boredapi.com/api/activity')
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.then(function (response){
+// 		dateAPIObject = response;
+// 		console.log(dateAPIObject);
+// 	})
+// 	.catch(err => console.error(err));
+
+// Joke API function
+getJoke(testCategories);
+function getJoke (categories) {
+	var jokeSectionJokeEl = $('#jokeSectionJoke');
+	
+	const jokeAPI = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Host': 'https://v2.jokeapi.dev/',
+			'X-RapidAPI-Key': ''
+		}
+	};
+
+	fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit', jokeAPI)
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.then(function (data) {
+			jokeAPIObject = data;
+			console.log(jokeAPIObject);
+		})
+		.catch(err => console.error(err));
+		jokeSectionJokeEl.value
+}
 
 // progress bar
 function updateProgressBar(progressBar, value) {
 	progressBar.querySelector(".progress-fill").style.width = `${value}%`;
 	progressBar.querySelector(".progress-text").textContent = `${value}%`;
-  }
+}
   
   const myProgressBar = document.querySelector(".progress");
   // test, need to pass love value to replace 55
   updateProgressBar(myProgressBar, 55);
-
-
-document.getElementById('#btn').addEventListener(onclick, updateProgressBar);
